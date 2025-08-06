876851a29d67dbec8be9688552aab924ca34068b
Shravanthi <dasisravanthi4@gmail.com>
2025-08-06 20:03:25 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,1 +1,14 @@
+tasks = []
 
+while True:
+    choice = input("\nAdd/View/Exit: ").lower()
+
+    if choice == "add":
+        tasks.append(input("Enter task: "))
+    elif choice == "view":
+        for i, t in enumerate(tasks, 1):
+            print(f"{i}. {t}")
+    elif choice == "exit":
+        break
+    else:
+        print("Invalid option.")
